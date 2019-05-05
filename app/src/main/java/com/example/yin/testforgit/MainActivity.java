224870a151bd8ea7ce9fb9b3a78689625502922c
Yin <yin@gmail.com>
2019-05-05 11:13:51 +0800

    AS修改

--- app/src/main/java/com/example/yin/testforgit/MainActivity.java
+++ app/src/main/java/com/example/yin/testforgit/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.yin.testforgit;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-		private TextView textView;
+    private TextView textView;
     @Override
     public Looper getMainLooper() {
         return super.getMainLooper();
